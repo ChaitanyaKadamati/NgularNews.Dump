c735c1343b14cdf12dd4504ad559805b6bcf5300
chaitanyakadamati <kskchaitanya@gmail.com>
2019-11-28 00:34:16 +0530

    first commit

---
+++ src/app/container/widget-news/widget-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-news',
+  templateUrl: './widget-news.component.html',
+  styleUrls: ['./widget-news.component.css']
+})
+export class WidgetNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
